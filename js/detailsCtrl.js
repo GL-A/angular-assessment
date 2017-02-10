5285d5e39251f868e07dd4bd17bb5b851588f433
George Lopez-Ayala <georgelopezayala@icloud.com>
2017-02-09 20:33:38 -0700

    dd

--- js/detailsCtrl.js
+++ js/detailsCtrl.js
@@ -1,11 +1,14 @@
 angular.module('app').controller('detailsCtrl', function($scope, service, $stateParams){
 
 
-  // var getOne = function() {
-  //   $scope.one = service.getOne($stateParams.id);
-  //   console.log($scope.one , 'hello')
-  // }
-  // getOne();
+  var getOne = function() {
+    $scope.one = service.getOne($stateParams.id).then(function(res){
+      $scope.apples = res;
+      console.log(res);
+    })
+    // console.log($scope.one , 'hello')
+  }
+  getOne();
 
   service.getOne($stateParams.id).then(function(res){
     $scope.one = res;
